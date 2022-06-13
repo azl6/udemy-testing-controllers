ba6f71ae13a6ec8ab2daafcaa3a92af499e62a4f
Alex <alex.rodrigues23997@gmail.com>
2022-06-13 20:27:06 -0300

    183: Testing list size and content on controllers' response

--- src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
@@ -2,6 +2,7 @@ package guru.springframework.brewery.web.controllers;
 
 import guru.springframework.brewery.services.BeerService;
 import guru.springframework.brewery.web.model.BeerDto;
+import guru.springframework.brewery.web.model.BeerPagedList;
 import guru.springframework.brewery.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,14 +10,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -29,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class BeerControllerTest {
 
     MockMvc mockMvc;
+
     @InjectMocks
     BeerController beerController;
 
@@ -37,6 +42,9 @@ class BeerControllerTest {
 
     @Mock
     BeerService beerService;
+
+    @Mock
+    BeerPagedList beersPaged;
 
 
     @BeforeEach
@@ -49,11 +57,13 @@ class BeerControllerTest {
                 .price(new BigDecimal("10"))
                 .build();
 
+        List<BeerDto> beers = List.of(beerDto);
+        beersPaged = new BeerPagedList(beers, PageRequest.of(1, 1), 2);
         mockMvc = MockMvcBuilders.standaloneSetup(beerController).build();
     }
 
     @Test
-    void getBeerById() throws Exception {
+    void getBeerByIdTest() throws Exception {
         given(beerService.findBeerById(any())).willReturn(beerDto);
 
         mockMvc.perform(
@@ -64,5 +74,16 @@ class BeerControllerTest {
                 .andExpect(jsonPath("$.beerName").value("Brahma"));
 
         then(beerService).should(times(1)).findBeerById(any());
+    }
+
+    @Test
+    void listBeersTest() throws Exception {
+        given(beerService.listBeers(any(), any(), any())).willReturn(beersPaged);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(1)))
+                .andExpect(jsonPath("$.content[0].beerName", is("Brahma")))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
     }
 }
